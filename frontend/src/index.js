da81b173a0c8f82fa787de90951972d803d5ad71
bryan0904 <87603335+bryan0904@users.noreply.github.com>
2025-07-06 23:21:59 +0800

    Add files via upload

---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';  // 如果沒有自訂樣式，可留空
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
